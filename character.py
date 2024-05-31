c09ee2de4b8c43b86df130f07fe54e3957bffbbf
Rbn <rubenavalosmtz@gmail.com>
2024-05-31 10:00:27 -0600

    Character class created

---
+++ character.py
@@ -0,0 +1,5 @@
+class Character:
+    def __init__(self, name, characteristics):
+        self.name = name
+        self.characteristicics = characteristics #list of the characteristics
+        self.down = False #this bools checks if the character is no longer selectable
